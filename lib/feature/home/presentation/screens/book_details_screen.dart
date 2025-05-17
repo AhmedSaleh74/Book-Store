6e59e7c1a74eabb1aaaffc1af7e1ab91f4686f58
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-05-17 18:30:45 +0300

    feat(ui): create BookDetailsScreen

---
+++ lib/feature/home/presentation/screens/book_details_screen.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/feature/home/presentation/widgets/book_details_screen_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsScreen extends StatelessWidget {
+  const BookDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BookDetailsScreenBody();
+  }
+}
