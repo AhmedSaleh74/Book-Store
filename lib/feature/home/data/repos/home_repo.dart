543f0e077c5c8ab6637e9b8d1307c80b062b0065
AhmedSaleh74 <AhmedSaleh1112001@gmail.com>
2025-05-18 19:42:22 +0300

    feat(repo): create home repository methods and errors file in core file

--- lib/feature/home/data/repos/home_repo.dart
+++ lib/feature/home/data/repos/home_repo.dart
@@ -1,7 +1,9 @@
+import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/feature/home/data/models/book_model.dart';
+import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
   //بحد الدوال هتعمل اي بس مبقولش هتعمله ازاي
-  Future<List<BookModel>> fetchFeaturedBooks();
-  Future<List<BookModel>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
 }
